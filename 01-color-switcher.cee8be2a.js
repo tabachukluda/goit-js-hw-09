fe3547037ad15ee41c926ce5b3f2a8112ea8cb57
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-06-03 18:35:53 +0000

    Deploying to gh-pages from @ tabachukluda/goit-js-hw-09@80979a2f44482cd3835941e95ae95bc5b7cafe14 🚀

---
+++ 01-color-switcher.cee8be2a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=null;var n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.cee8be2a.js.map
